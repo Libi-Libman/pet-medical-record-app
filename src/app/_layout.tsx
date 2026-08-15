caa4a57897e547ad553d61f8a1ae9ffac2fc01b8
Libi Libman <libi.libman@gmail.com>
2026-08-15 20:36:03 +0200

    project cleanup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,15 +3,12 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 import '../../global.css';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
       <Stack screenOptions={{ headerShown: false }} />
     </ThemeProvider>
   );
